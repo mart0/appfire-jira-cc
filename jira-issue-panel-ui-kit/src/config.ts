ea12e306ee7eff4e1d38f1f2903a125b00c33573
Martin Marinov <martin.mari@sbtech.com>
2025-03-10 14:34:04 +0200

    add env vars

--- jira-issue-panel-ui-kit/src/config.ts
+++ jira-issue-panel-ui-kit/src/config.ts
@@ -1,6 +1,6 @@
 // Replace these with your actual credentials
 export const JIRA_CONFIG = {
-  EMAIL: 'marinov9210@gmail.com', // Replace with your Jira email
-  API_TOKEN: 'ATATT3xFfGF0ejL7lCSbKQcKSfpJFaueoeOQJi7TtvyuBAlHLEDRBWGVe7c1zMUsyVu8kdSV-Un2MZk9-0gh0zi4OQtLZ2nx7LpOXsWqJABzJGrDmxCDhEQIx9u9D4PLv5as48n4swf6XeY69uVgay7HdLnKQJ-N_36F_uCD67WDzKfCJOkP6PM=53F76937', // Replace with your Jira API token
-  BASE_URL: 'https://appfire-mmarinovm.atlassian.net'
+  EMAIL: process.env.EMAIL || '', // Replace with your Jira email
+  API_TOKEN: process.env.API_TOKEN || '', // Replace with your Jira API token
+  BASE_URL: process.env.BASE_URL || ''
 };
